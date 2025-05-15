9abc17b1532b2e91eaa0fc17db3bfe7bf7c25b36
Prem Kumar <premkumar.sridhar021@gmail.com>
2025-05-15 10:34:47 +0530

    'updated'

---
+++ src/app/timezone.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+
+export class TimezoneService {
+  private baseUrl = 'https://localhost:7065/api/time'; // Backend API base URL
+
+  constructor(private http: HttpClient) {}
+
+  getTimezones(): Observable<string[]> {
+    return this.http.get<string[]>(`${this.baseUrl}/timezones`);
+  }
+
+  // Fetch the selected timezone time
+  getSelectedTime(timezone: string): Observable<{ selectedTime: string }> {
+    return this.http.get<{ selectedTime: string }>(
+      `${this.baseUrl}?timezone=${timezone}`
+    );
+  }
+}
